cb34511a806b5225de6bac1c6e5476339b5d76a7
mayonnaise1052 <clacla919a@gmail.com>
2026-07-05 19:24:05 +0900

    remove tutorial

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -2,7 +2,6 @@ import { redirect } from "next/navigation";
 
 import { createClient } from "@/lib/supabase/server";
 import { InfoIcon } from "lucide-react";
-import { FetchDataSteps } from "@/components/tutorial/fetch-data-steps";
 import { Suspense } from "react";
 
 async function UserDetails() {
@@ -44,7 +43,6 @@ export default function ProtectedPage() {
           </a>
           .
         </p>
-        <FetchDataSteps />
       </div>
     </div>
   );
